39e4d1dfdf698d6477c21ccd75acb09bcb3bc89c
Ishan Dutta <ishandutta2007@gmail.com>
2022-12-18 14:24:12 +0530

    3c

--- createfile.py
+++ createfile.py
@@ -1,7 +1,7 @@
 with open(files.txt) as f:
     lines = f.readlines()
     for line in lines:
-    print(line)
-    line = line.replace("\n")
-    with open(line, 'w') as f:
-        f.write('//dummy file ' + line )
+        print(line)
+        line = line.replace("\n")
+        with open(line, "w") as f:
+            f.write("//dummy file " + line)
