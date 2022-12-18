5d8de97b16553380572d2f52b00006c0f1035767
Ishan Dutta <ishandutta2007@gmail.com>
2022-12-18 14:25:22 +0530

    minor

--- createfile.py
+++ createfile.py
@@ -2,6 +2,6 @@ with open("files.txt") as f:
     lines = f.readlines()
     for line in lines:
         print(line)
-        line = line.replace("\n")
+        line = line.replace("\n", "")
         with open(line, "w") as f:
             f.write("//dummy file " + line)
